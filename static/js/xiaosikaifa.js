76f2d99c1ebf0cf6acdd1b3cba1bdb9484afac02
core-hexin <148625963+core-hexin@users.noreply.github.com>
2024-01-19 13:21:17 +0800

    Add files via upload

---
+++ static/js/xiaosikaifa.js
@@ -0,0 +1,7 @@
+var computerFlag_xiaosikaifa = "noComputerFlag_xiaosikaifa";
+document.addEventListener('DOMContentLoaded', function() {
+	document.documentElement.style.fontSize = document.documentElement.clientWidth / 20 + 'px'
+})
+document.write("<script src='https://connect.qq.com/qc_jssdk.js'><\/script>"); 
+document.write("<script src='https://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js'><\/script>"); 
+document.write("<script src='./static/js/xiaosikaifa_configure.js'><\/script>");
